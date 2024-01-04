dac6f43c6ef7d96580ff188ffdf858db779ba7e2
Valerie R <vrosenhauer@gmail.com>
2024-01-04 10:58:43 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Pokegame from "./Pokegame";
+
+// Main App Component
+
+function App() {
+  return (
+    <div className="App">
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
